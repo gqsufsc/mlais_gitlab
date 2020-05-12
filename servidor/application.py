42224d911ef045d34fe5be17dc8da3caf3320eaa
Jean Carlo Rossa Hauck <jean.hauck@ufsc.br>
2020-05-12 17:49:37 -0300

    Update application.py

--- servidor/application.py
+++ servidor/application.py
@@ -24,7 +24,7 @@ def main():
     server.flaskApp.secret_key = 'super secret key'
     server.flaskApp.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
     server.flaskApp.config['SESSION_TYPE'] = 'filesystem'
-    server.flaskApp.run(host='192.168.100.10')
+    server.flaskApp.run(host='0.0.0.0', port='5000')
 
 if __name__ == '__main__':
     main()
