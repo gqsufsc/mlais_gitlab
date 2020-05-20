688d7a500d06826b5932baae477fd64f2d37690d
Lucas Martins Petroski <petroski31@gmail.com>
2020-05-20 11:32:22 -0300

    Tcc/lucas.martins

--- servidor/static/js/turma.js
+++ servidor/static/js/turma.js
@@ -1,3 +1,17 @@
-$(document).ready(function () {
+function confirmModal(obj){
+    $('#hidden_input').val(obj.id);
+    $("#confirm-modal").show();
+}
 
-});
+function closeModal() {
+    $("#confirm-modal").modal('hide');
+}
+
+function zoomOut() {
+    $("#zoom-modal").modal('hide');
+}
+
+function zoomModal(obj) {
+  document.getElementById("img").src = obj.src;
+  $('#zoom-modal').show();
+}
